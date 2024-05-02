3b74ca4158b736b298189b512b5a9cf2f7ac20cd
Evan <evanchapela@gmail.com>
2024-05-02 19:48:40 +0200

    build(ConjuredItem): add base class

--- app/src/main/java/edu/badpals/item/ConjuredItem.java
+++ app/src/main/java/edu/badpals/item/ConjuredItem.java
@@ -1,2 +1,37 @@
-package edu.badpals.item;public class ConjuredItem {
+package edu.badpals.item;
+
+public class ConjuredItem extends Item implements Updateable{
+
+    public ConjuredItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public int getQuality() {
+        return this.quality;
+    }
+
+    @Override
+    public int getSellIn() {
+        return this.sellIn;
+    }
+
+    @Override
+    public void update_item() {
+        this.sellIn -= 1;
+    }
+
+    @Override
+    public void update_quality() {
+
+        if (this.sellIn <= 0){
+            this.quality -= 4 ;
+        } else {
+            this.quality -= 2 ;
+        }
+        if (this.quality < 0) {
+            this.quality = 0;
+        }
+
+    }
 }
--- app/src/test/java/edu/badpals/item/ConjuredItemTest.java
+++ app/src/test/java/edu/badpals/item/ConjuredItemTest.java
@@ -16,49 +16,49 @@ public class ConjuredItemTest {
     @Test
     public void testQualityUpdate(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", 3, 6);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(4,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateOverZero(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", 10, 1);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(0,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateZero(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", 0, 6);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(2,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateUnderZero(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", -1, 6);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(2,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateUnderZero3(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", -1, 3);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(0,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateUnderZero2(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", -1, 2);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(0,zero.getQuality()) ;
     }
 
     @Test
     public void testQualityUpdateUnderZero1(){
         ConjuredItem zero = new ConjuredItem("Conjured Mana Cake", -1, 1);
-        zero.update_item();
+        zero.update_quality();
         assertEquals(0,zero.getQuality()) ;
     }
 }
