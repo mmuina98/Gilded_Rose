66dd9015b80992d31b9badd2ad906e7c707c8c8a
Evan <evanchapela@gmail.com>
2024-05-02 19:33:15 +0200

    build(Sulfuras)finish base class

--- app/src/main/java/edu/badpals/item/Sulfuras.java
+++ app/src/main/java/edu/badpals/item/Sulfuras.java
@@ -1,2 +1,28 @@
-package edu.badpals.item;public class Sulfuras {
+package edu.badpals.item;
+
+public class Sulfuras extends Item implements Updateable{
+
+    public Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public int getQuality() {
+        return this.quality;
+    }
+
+    @Override
+    public int getSellIn() {
+        return this.sellIn;
+    }
+
+    @Override
+    public void update_item() {
+        // https://www.youtube.com/watch?v=mLyOj_QD4a4
+    }
+
+    @Override
+    public void update_quality() {
+        // LEEEEEEEEEEEEEEEEEEEEEEEEEEROY JEEEEEEEEEEEEEEEEEENKINS
+    }
 }
